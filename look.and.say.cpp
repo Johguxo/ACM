60a600916ceb0f8c4aeb5cbf511f4d6bde4e9249
Johguxo <37115423+Johguxo@users.noreply.github.com>
2018-03-08 21:13:38 -0500

    Update look.and.say.cpp

--- look.and.say.cpp
+++ look.and.say.cpp
@@ -1,31 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector <int> numbers;
-string n,p,repet;
-int cont;
+vector <string> numbers;
+string n,repet,p;
+int cont,i,j;
 
 int main(){
 	while(cin>>p>>i>>j){
-	if(p == 0 && i==0 && j == 0) break;
+		if(p == "0" && i==0 && j == 0) break;
 		numbers.push_back(p);
-		for(int h = 1; h <= 1010;h++){
-			t = p.size();
-			while(t >= 0){
+		for(int h = 1; h <= i+10;h++){
+			
+			int t = p.size();
+			string aux="";
+			int cifras = 0;
+			while(t > 0 && cifras<=10000){
 				cont = 1;
 				repet = p[t-1];
+				
 				while(p[t-1] == p[t-2]){
 					cont++;
+					cifras++;
 					t--;
 				}
 				t--;
-				p = repet + to_string(cont);
+				cifras ++;
+				aux = to_string(cont) + repet + aux ;
 			}	
-			numbers.push_back(p);					
+			numbers.push_back(aux);
+			cout << numbers[h-1] << endl;
+			p = aux;					
 		}
-	n  = to_string(numbers[i-1]);
-	cout<<n[j-1]<<endl;
-	numbers.clear();
+		n  = numbers[i-1];
+		cout<<n[numbers[i-1].size()-j-1]<<endl;
+		numbers.clear();
 	}
 	return 0;
 }
-
